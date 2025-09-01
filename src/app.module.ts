c9dd9bf67c5e8c807bcff0cc8179a0e4129c3cf7
Vuong Vu <vuongvu061101@gmail.com>
2025-09-01 23:49:09 +0700

    config mysql database and typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,48 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import Joi from 'joi'
+import path from 'path'
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { CategoryModule } from './modules/category/category.module'
+import { ProductModule } from './modules/product/product.module'
+import { UserModule } from './modules/user/user.module'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // injected into all modules
+      validationSchema: Joi.object({
+        PORT: Joi.number().required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_NAME: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.getOrThrow('DB_HOST'),
+        port: configService.getOrThrow('DB_PORT'),
+        username: configService.getOrThrow('DB_USERNAME'),
+        password: configService.getOrThrow('DB_PASSWORD'),
+        database: configService.getOrThrow('DB_NAME'),
+        entities: [path.join(__dirname, 'entities', '*.entity.{ts,js}')],
+        synchronize: false,
+        logging: true,
+        namingStrategy: new SnakeNamingStrategy(),
+      }),
+    }),
+    CategoryModule,
+    ProductModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
