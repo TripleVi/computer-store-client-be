3cb0432a4a1f012991aba0c2e4c23fac601da766
Vuong Vu <vuongvu061101@gmail.com>
2025-09-01 23:50:30 +0700

    create user, category and product modules

---
+++ src/modules/category/category.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common'
+// import { ConfigService } from '@nestjs/config'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Category } from 'src/entities'
+import { Repository } from 'typeorm'
+
+@Injectable()
+export class CategoryService {
+  constructor(
+    @InjectRepository(Category) private readonly categoryRepo: Repository<Category>,
+    // private readonly configService: ConfigService
+  ) {}
+
+  findAll() {
+    return this.categoryRepo.find()
+  }
+}
